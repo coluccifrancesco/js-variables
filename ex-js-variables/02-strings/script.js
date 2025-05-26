0b845483a6fe9a8879c1e318429616fdfa5c4d53
coluccifrancesco <coluccif2002@gmail.com>
2025-05-26 15:10:45 +0200

    2.4 done

--- ex-js-variables/02-strings/script.js
+++ ex-js-variables/02-strings/script.js
@@ -30,3 +30,5 @@ console.log(fullName); // Output: "Jane Smith"
 // sulla variabile alphabet per ottenere la sua lunghezza
 const numberOfLettersInAlphabet = null;
 console.log(numberOfLettersInAlphabet); // Output: 26
+
+console.log(alphabet.length)
