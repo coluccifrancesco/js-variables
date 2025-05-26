6be6746d7446a70a439f78a80f4e384e844bb6ac
coluccifrancesco <coluccif2002@gmail.com>
2025-05-26 14:58:25 +0200

    Numbers exercises done

--- ex-js-variables/01-numbers/script.js
+++ ex-js-variables/01-numbers/script.js
@@ -9,24 +9,39 @@ const numThree = 32; // NON MODIFICARE QUESTA RIGA
 const numOnePlusNumTwo = NaN;
 console.log(numOnePlusNumTwo); // Output: 24
 
+console.log(numOne + numTwo)
+
+
 // 2. Imposta questa variabile come prodotto di numThree e numTwo
 const numThreeTimesNumTwo = NaN;
 console.log(numThreeTimesNumTwo); // Output: 512
+
+console.log(numThree * numTwo);
+
 
 // 3. Imposta questa variabile come divisione di numThree per numOne
 const numThreeDividedByNumOne = NaN;
 console.log(numThreeDividedByNumOne); // Output: 4
 
+console.log(numThree / numOne);
+
+
 // 4. Imposta questa variabile come differenza tra numThree e numOne
 const numThreeMinusNumOne = NaN;
 console.log(numThreeMinusNumOne); // Output: 24
+
+console.log(numThree - numOne);
+
 
 // 5. Imposta questa variabile come somma di numOne, numTwo e numThree
 const sum = NaN;
 console.log(sum); // Output: 56
 
-console.log(numOne + numTwo + numThree)
+console.log(numOne + numTwo + numThree);
+
 
 // 6. Imposta questa variabile come somma di numOne, numTwo, numThree diviso numOne
 const numBytes = NaN;
 console.log(numBytes); // Output: 7
+
+console.log((numOne + numTwo + numThree)/ numOne);
